b4a8bf53d636631ceb82e6b4a50354b8282c4ab1
Raclsc <aclscmj1107@gmail.com>
2025-08-13 12:32:02 +0800

    Add program comments.

--- ZhuyinFixer.py
+++ ZhuyinFixer.py
@@ -1,28 +1,27 @@
 # Zhuyin Fixer 
-# V輸入亂碼 > V比對mapping > V逐個轉換成注音 > V根據音標分割單字 > 比對Dictionary > 轉換成中文詞 > 組合成一句話 > 輸出結果
-
-'''
-假設7個字"ji3vu;3ul4t 1u/ fu6xup6"，猜分成單字["ji3", "vu;3", "ul4", "t ", "1u/ ", "fu6", "xup6"],
-根據按鍵位置轉換成注音符號['ㄨㄛˇ', 'ㄒㄧㄤˇ', 'ㄧㄠˋ', 'ㄔ-', 'ㄅㄧㄥ-', 'ㄑㄧˊ', 'ㄌㄧㄣˊ'], 
-以最大字數嘗試轉換成繁體中文詞語, [0:7]比對失敗, [0:6]比對失敗, [0:5]比對失敗......, [0:2]比對失敗, [0:1]比對出中文字["我"],
-[i,j], i = 0 ~ 7(max length), j = 7 ~ 1,
-接著第二次從i = 1 ~ 7, j = 7 ~ 2, [1:7]比對失敗, [1:6]比對失敗......, [1:4]比對失敗, [1:3]比對出中文字["想要"],
-接著第三次從i = 3 ~ 7, j = 7 ~ 4, [3:7]比對失敗, [3:6]比對失敗, [3:5]比對失敗, [3:4]比對出中文字["吃"],
-接著第四次從i = 4 ~ 7, j = 7 ~ 5, [4:7]比對出中文字["冰淇淋"],
-最後組合所有詞語並輸出"我想要吃冰淇淋"
-'''
+# 功能:
+#       將忘記切換輸入法導致的英文亂碼轉換為注音符號，
+#       再將注音符號轉換成繁體中文。
+#       支援詞語配對，盡可能地將注音轉換成完整詞語。
 
 import json
+import tkinter as tk
 
+
+def ChineseDictFile():
+    """讀取中文字詞典檔案"""
+    with open("ChineseDictionary.json", "r", encoding="utf-8") as f:
+        return json.load(f)
 
 def fix2zhuyin(text):
+    """將亂碼字串轉換成注音符號列表，並以聲調當作分界"""
     with open("Zhuyin_mapping.json", "r", encoding="utf-8") as f:
         mapping = json.load(f)
         
     word = ""
     result = []
     for char in text:
-        if char in [" ", "6", "3", "4", "7"]:
+        if char in [" ", "6", "3", "4", "7"]:   # 聲調，空格指一聲(陰平聲)
             word += mapping.get(char, f"{char}")
             result.append(word)
             word = ""
@@ -34,11 +33,9 @@ def fix2zhuyin(text):
         
     return result
 
-def ChineseDictFile():
-    with open("ChineseDictionary.json", "r", encoding="utf-8") as f:
-        return json.load(f)
         
 def WordSplit(wordlist):
+    """使用最大匹配演算法分割注音成詞語"""
     i = 0
     length = len(wordlist)
     result = []
@@ -53,13 +50,14 @@ def WordSplit(wordlist):
                 matched = True
                 break
                 
-        if not matched:
+        if not matched:     # 無匹配時，當作單字
             result.append(wordlist[i])
             i += 1
             
     return result
     
 def fix2Chinese(zhuyinWords):
+    """將分割後的注音詞語轉換成繁體中文詞語"""
     text = ""
     for word in zhuyinWords:
         text += ChineseDict.get(word, f"{word}")
